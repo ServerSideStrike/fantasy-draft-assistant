76a8982a2a1b2a254c1fbc9973979b8276d2dc09
ServerSideStrike <matkins1812@gmail.com>
2026-09-06 15:21:34 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="fantasy-draft-assistant-v2.4";
+const CACHE="fantasy-draft-assistant-v2.4.2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(
